3b9d5690349c197863e2469458b272ef0062e4c9
ekohadipermana <ekohadipermana@gmail.com>
2023-11-08 12:02:01 +0700

    return a dictionary

--- main.py
+++ main.py
@@ -724,3 +724,13 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name('jimi', 'hEndriX')
 print(musician)
+
+# Returning a Dictionary, p 139
+
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person"""
+    person = {'first': first_name.title(), 'last': last_name.title()}
+    return person
+
+musician = build_person('jImI', 'hendRix')
+print(musician)
