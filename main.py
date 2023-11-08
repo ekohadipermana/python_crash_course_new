0c8c4db84842e130478b95fbfcc7d63e3795c725
ekohadipermana <ekohadipermana@gmail.com>
2023-11-08 11:54:28 +0700

    return values

--- main.py
+++ main.py
@@ -715,3 +715,12 @@ describe_origin('Aragorn', "gondor")
 
 # start learning 20231108, wed, 11.35
 
+# page 137, Return Values
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name neatly formatted"""
+    full_name = f'{first_name} {last_name}'
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hEndriX')
+print(musician)
